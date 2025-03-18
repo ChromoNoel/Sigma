dc8a4c845f5825a8be1f6446208b81f801709c0e
12c-kormendi <12c-kormendi@I202-003011>
2025-03-18 09:16:39 +0100

    Add project files.

---
+++ PointsOfAuthority/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointsOfAuthority
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            kapcsolo.Visibility = Visibility.Collapsed;
+        }
+
+        private void checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            kapcsolo.Visibility = Visibility.Visible;
+        }
+
+        private void checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            kapcsolo.Visibility = Visibility.Collapsed;
+        }
+    }
+}
